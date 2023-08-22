feda9aeea6b7986950675e54d1531f592f6f6820
Ranadeep Biswas <mail@rnbguy.at>
2023-08-23 00:05:22 +0200

    fix data race

--- src/astral.ts
+++ src/astral.ts
@@ -22,11 +22,10 @@ export class MarauderAstral {
           `--proxy-server=${this.proxy.proxyUrl()}`,
         ],
       });
+      this.proxy?.start();
     } else {
       this.browser = await launch();
     }
-
-    return this;
   }
 
   async resolveDoiLink(doi: string): Promise<URL> {
@@ -46,79 +45,66 @@ export class MarauderAstral {
   }
 
   async pdfLinks(doiUrl: URL): Promise<URL[]> {
-    this.proxy?.start();
-    await new Promise((resolve) => setTimeout(resolve, 1000));
+    const page = await this.browser?.newPage();
+    //Allow JS.
+    // await page?.setJavaScriptEnabled(true);
 
-    try {
-      const page = await this.browser?.newPage();
-      //Allow JS.
-      // await page?.setJavaScriptEnabled(true);
+    await page?.goto(doiUrl.toString(), { waitUntil: "networkidle2" });
 
-      await page?.goto(doiUrl.toString(), { waitUntil: "networkidle2" });
+    let pdfLinks: string[] = await page?.evaluate(() => {
+      return Array.from(document.querySelectorAll("a")).map(
+        (el) => el?.getAttribute("href"),
+      ).filter((v) => v?.includes("pdf"));
+    });
 
-      let pdfLinks: string[] = await page?.evaluate(() => {
-        return Array.from(document.querySelectorAll("a")).map(
-          (el) => el?.getAttribute("href"),
-        ).filter((v) => v?.includes("pdf"));
-      });
+    await page?.close();
 
-      await page?.close();
+    pdfLinks = pdfLinks?.map((link) => new URL(link, doiUrl).href);
+    pdfLinks = [...new Set(pdfLinks)];
+    pdfLinks.sort();
 
-      pdfLinks = pdfLinks?.map((link) => new URL(link, doiUrl).href);
-      pdfLinks = [...new Set(pdfLinks)];
-      pdfLinks.sort();
+    log.debug("DOI page has PDFs:", pdfLinks);
 
-      log.debug("DOI page has PDFs:", pdfLinks);
-
-      return pdfLinks.map((link) => new URL(link));
-    } finally {
-      this.proxy?.end();
-    }
+    return pdfLinks.map((link) => new URL(link));
   }
 
   async downloadPdf(pdfUrl: URL, original: URL): Promise<Uint8Array> {
-    this.proxy?.start();
-    await new Promise((resolve) => setTimeout(resolve, 1000));
+    const page = await this.browser?.newPage();
 
-    try {
-      const page = await this.browser?.newPage();
+    await page?.goto(original.toString(), { waitUntil: "networkidle2" });
 
-      await page?.goto(original.toString(), { waitUntil: "networkidle2" });
+    const base64Data = await page?.evaluate(
+      async (downloadUrl: string) => {
+        const resp = await fetch(downloadUrl, { credentials: "include" });
 
-      const base64Data = await page?.evaluate(
-        async (downloadUrl: string) => {
-          const resp = await fetch(downloadUrl, { credentials: "include" });
+        // puppeteer's page.evaluate() runs in browser context
+        // and communicates via json-serializable values
+        // we need to convert the response body to a base64 string
 
-          // puppeteer's page.evaluate() runs in browser context
-          // and communicates via json-serializable values
-          // we need to convert the response body to a base64 string
+        // fastest browser native solution to convert large binary data to base64
+        // https://stackoverflow.com/a/66046176
 
-          // fastest browser native solution to convert large binary data to base64
-          // https://stackoverflow.com/a/66046176
-
-          async function bufferToBase64(blob: Blob) {
-            // use a FileReader to generate a base64 data URI:
-            const base64url: string = await new Promise((resolve) => {
-              const reader = new FileReader();
-              reader.onload = () => resolve(reader.result);
-              reader.readAsDataURL(blob);
-            });
-            return base64url.substring(base64url.indexOf(",") + 1);
-          }
-          return await bufferToBase64(await resp.blob());
-        },
-        {
-          args: [pdfUrl.toString()],
-        },
-      );
-      await page?.close();
-      return b64Decode(base64Data);
-    } finally {
-      this.proxy?.end();
-    }
+        async function bufferToBase64(blob: Blob) {
+          // use a FileReader to generate a base64 data URI:
+          const base64url: string = await new Promise((resolve) => {
+            const reader = new FileReader();
+            reader.onload = () => resolve(reader.result);
+            reader.readAsDataURL(blob);
+          });
+          return base64url.substring(base64url.indexOf(",") + 1);
+        }
+        return await bufferToBase64(await resp.blob());
+      },
+      {
+        args: [pdfUrl.toString()],
+      },
+    );
+    await page?.close();
+    return b64Decode(base64Data);
   }
 
   async close() {
+    await this.proxy?.end();
     await this.browser?.close();
   }
 }
